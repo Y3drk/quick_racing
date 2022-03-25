b5883687ceb316be23c73ca43f7e1d3bd1598509
Y3drk <ziebura.jedrzej@gmail.com>
2022-03-25 20:20:38 +0100

    Basics of surfaces added

---
+++ surface.py
@@ -0,0 +1,18 @@
+import pygame as pg
+from vector2d import Vector2D
+from __future__ import annotations
+from surfaceType import SurfaceType
+
+
+class Surface(pg.sprite.Sprite):
+    def __init__(self, position: Vector2D, width: int, height: int, fraction: SurfaceType.value):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((width, height))
+        self.image.fill((255, 255, 255)) #for now it's all white but we should figure out how to colour it differently
+        self.image.set_colorkey((0, 0, 0)) #this will make the img ignore all the white pixels
+        self.rect = self.image.get_rect()
+        self.rect.x = position.x
+        self.rect.y = position.y
+        self.width = width
+        self.height = height
+        self.fraction = fraction
---
+++ surfaceType.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class SurfaceType(Enum):
+    ASPHALT = 0.9
+    SNOW = 0.35
+    ICE = 0.1
+    GRAVEL = 0.4
+    GRASS = 0.5
+    SAND = 0.25
+
